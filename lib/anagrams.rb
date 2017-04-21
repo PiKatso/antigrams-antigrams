5550d5b4c1d7921f94c0f9811c7e5ed2a011feed
Kat Gifford <katgiff2007@gmail.com>
2017-04-21 11:47:51 -0700

    updated a few items on spec file and created palidrome method using reverse. not passing all three specs

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,13 +3,20 @@ class String
   define_method(:anagram?) do |word|
      if(self.downcase.chars.sort == word.downcase.chars.sort)
         true
-        else
+        return "it is an anagram"
+      else
         false
+        return "it is not an anagram"
       end
     end
 
-  def palidrome?(word)
-
-
+  def palidrome?
+    if self.downcase == self.downcase.reverse
+      true
+      "it is a palidrome"
+    else
+      false
+      "it is not a palidrome"
+    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,17 +2,19 @@ require 'rspec'
 require 'anagrams'
 
 
-describe 'String#anagram' do
+describe 'String#anagram?' do
   # first spec
   it 'takes user input and evaluates if input is valid word' do
-    expect('cat'.anagram).to(eq 'it is an anagram')
+    expect('cat'.anagram?('tac')).to(eq 'it is an anagram')
   end
   # second spec
   it 'takes user input and recognizes regardless of case' do
-    expect('CaT'.method_name).to(eq 'it is an anagram')
+    expect('CaT'.anagram?('tAc')).to(eq 'it is an anagram')
   end
-  # third spec
+end
+
+  describe 'String#palidrome?' do
   it 'if it is an anagram, also check if input is a palidrome' do
-    expect('cat'.method_name).to(eq 'it is  not a palidrome')
+    expect('cat'.palidrome?).to(eq 'it is not a palidrome')
   end
 end
