f4c52c12e55bf39a41dd2c789b8ebc1b595f2bb5
Kat Gifford <katgiff2007@gmail.com>
2017-04-26 22:14:49 -0700

    passing last spec, sinatra integration spec. had formerly had quotes around 'page'

--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -6,9 +6,9 @@ set(:show_exceptions, false)
 describe('the application path', {:type => :feature}) do
   it 'it processes the user entry and returns antagram results as output' do
     visit('/')
-    fill_in('input1', :with => 'cat')
-    fill_in('input2', :with => 'tac')
+    fill_in('input1', :with => 'h3')
+    fill_in('input2', :with => 'hi')
     click_button('Find Out')
-    expect('page').to have_content ("it is an anagram")
+    expect(page).to have_content("please enter a valid english words. thx!")
   end
 end
